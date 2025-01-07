52d4da48bf391edfb2e24eac5c6fc91205ac038c
Evgeniy Valyaev <valyay@evilmartians.com>
2025-01-07 14:47:20 +0400

    Change news page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,33 +11,24 @@ export default function Page() {
 			<h1 className="mb-6 text-3xl font-bold">Latest News</h1>
 			<ul className="space-y-6">
 				{news.map((item: NewsMeta) => (
-					<li key={item.slug} className="rounded-lg bg-white p-4 shadow">
-						{item.imageUrl ? (
-							<div className="mb-6">
-								<Image
-									src={item.imageUrl}
-									alt={item.title}
-									width={800}
-									height={400}
-									className="rounded-md"
-								/>
-							</div>
-						) : (
-							<p className="italic text-gray-500">Image does not exist</p>
-						)}
-
-						<h2 className="mb-2 text-xl font-semibold">{item.title}</h2>
-
-						<p className="mb-2 text-gray-700">{item.excerpt}</p>
-
-						<span className="mb-4 block text-sm text-gray-500">
-							{item.date}
-						</span>
-
+					<li key={item.slug}>
 						<Link
 							href={`/${item.slug}`}
-							className="text-blue-600 hover:underline">
-							Read more
+							className="block rounded-lg bg-white p-4 shadow transition-[transform,box-shadow] duration-200 ease-in-out hover:scale-105 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-600">
+							<h2 className="mb-2 text-xl font-semibold">{item.title}</h2>
+							{item.imageUrl ? (
+								<div className="mb-6">
+									<Image
+										src={item.imageUrl}
+										alt={item.title}
+										width={800}
+										height={400}
+										className="rounded-md"
+									/>
+								</div>
+							) : (
+								<div className="mb-6 h-[400px] rounded-md bg-gradient-to-r from-gray-100 via-gray-200 to-gray-300"></div>
+							)}
 						</Link>
 					</li>
 				))}
